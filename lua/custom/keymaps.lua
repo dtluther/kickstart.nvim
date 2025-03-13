ff2357026372c9837d82f1e732b354f6100144ee
Dillon Luther <dillon.luther@gmail.com>
2025-03-13 11:23:48 -0400

    Pasting doesn't overwrite clipboard, move my paste commands into custom keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -12,3 +12,22 @@ vim.keymap.set('v', '<leader>R', '"hy:%s/<C-r>h//gc<Left><Left><Left>', {
   silent = false,
   desc = 'Replace highlighted text globally with confirmation',
 })
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  -- Use the system clipboard by default.
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- Remap visual mode paste to avoid overwriting the clipboard.
+-- Explanation:
+-- 1. When in visual mode ("x"), pressing 'p':
+--    - '"_d' deletes the selected text into the black hole register,
+--      which discards it instead of updating your clipboard.
+--    - 'P' pastes the content from your current clipboard before the cursor.
+-- 2. This ensures that you can press 'p' repeatedly to paste the same content,
+--    since the clipboard remains unchanged.
+vim.keymap.set('x', 'p', '"_dP', { noremap = true, silent = true })
